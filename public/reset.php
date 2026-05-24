59db707c3eaa32ebcab8bf742c64c4366a75e049
hana <j4nhana@gmail.com>
2026-05-24 14:12:09 +0700

    temp reset file2

--- public/reset.php
+++ public/reset.php
@@ -1,3 +1,7 @@
 <?php
+require_once '../config/config.php';
+require_once '../src/helpers/Database.php';
+
 $hash = password_hash('password123', PASSWORD_BCRYPT, ['cost' => 10]);
-echo $hash;
+Database::query("UPDATE users SET password=? WHERE id IN (1,14,15)", [$hash]);
+echo "Password berhasil direset! Hash: " . $hash;
